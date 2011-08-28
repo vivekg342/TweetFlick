de85a7a4321b2bfb58762fd536c4bc80e49ebe51
Vivek Aditya <vivekg342@gmail.com>
2011-08-28 10:28:02 +0530

    time ago

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-before_filter :set_time_zone
+#before_filter :set_time_zone
 helper_method :current_user
   helper_method :user_signed_in?
   helper_method :correct_user?
 
   private
-  def set_time_zone
+  # def set_time_zone
   #Time.zone = ActiveSupport::TimeZone.new('my timezone', cookies[:timezone])
  # min = cookies[:timezone].to_i
   #Time.zone = ActiveSupport::TimeZone[-min.minutes]
-  Time.zone = "New Delhi"
-end
+  #Time.zone = "New Delhi"
+  #end
     def current_user
       begin
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
