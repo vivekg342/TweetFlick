4c92e97cc3dd068b23e002a5b6346d81a6cfe7e6
Vivek Aditya <vivekg342@gmail.com>
2011-08-28 10:36:22 +0530

    time ago

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module Tweetflick
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'New Delhi'
-    config.active_record.default_timezone = :local
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
