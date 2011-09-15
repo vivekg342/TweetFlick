eab032e378b92a21472c9b5c43b5ba4ff70759ea
Vivek Aditya <vivekg342@gmail.com>
2011-09-15 17:27:24 +0530

    bundle

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '8AL72cho2nqFZ4PQERT8rQ', 'rM4PUPAbTNe9exOotqaTebI0oxP1WXknWX4m42dow'
   #provider :facebook, 'KEY', 'SECRET'
 end
-  Twitter.configure do |config|
-       config.consumer_key = '8AL72cho2nqFZ4PQERT8rQ'
-   config.consumer_secret = 'rM4PUPAbTNe9exOotqaTebI0oxP1WXknWX4m42dow'
-  end
+  
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,4 @@
+Twitter.configure do |config|
+       config.consumer_key = '8AL72cho2nqFZ4PQERT8rQ'
+   config.consumer_secret = 'rM4PUPAbTNe9exOotqaTebI0oxP1WXknWX4m42dow'
+  end
